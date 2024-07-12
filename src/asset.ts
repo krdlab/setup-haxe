2106137c8d22b0865be534f6476903d001b2c955
Aidan Lee <aidan.lee63@gmail.com>
2024-07-12 15:10:56 +0100

    use neko 2.4rc and universal bundle on mac

--- src/asset.ts
+++ src/asset.ts
@@ -100,12 +100,12 @@ abstract class Asset {
   }
 }
 
-// * NOTE https://github.com/HaxeFoundation/neko/releases/download/v2-3-0/neko-2.3.0-linux64.tar.gz
-// * NOTE https://github.com/HaxeFoundation/neko/releases/download/v2-3-0/neko-2.3.0-osx64.tar.gz
-// * NOTE https://github.com/HaxeFoundation/neko/releases/download/v2-3-0/neko-2.3.0-win64.zip
+// * NOTE https://github.com/HaxeFoundation/neko/releases/download/v2-4-0-rc-1/neko-2.3.0-rc.1-linux64.tar.gz
+// * NOTE https://github.com/HaxeFoundation/neko/releases/download/v2-4-0-rc-1/neko-2.4.0-rc.1-osx-universal.tar.gz
+// * NOTE https://github.com/HaxeFoundation/neko/releases/download/v2-4-0-rc-1/neko-2.3.0-rc.1-win64.zip
 export class NekoAsset extends Asset {
   static resolveFromHaxeVersion(version: string) {
-    const nekoVer = version.startsWith('3.') ? '2.1.0' : '2.3.0'; // Haxe 3 only supports neko 2.1
+    const nekoVer = version.startsWith('3.') ? '2.1.0' : '2.4.0-rc.1'; // Haxe 3 only supports neko 2.1
     return new NekoAsset(nekoVer);
   }
 
@@ -124,6 +124,10 @@ export class NekoAsset extends Asset {
     // No 64bit version of neko 2.1 available for windows
     if (this.env.platform === 'win' && this.version.startsWith('2.1')) {
       return this.env.platform;
+    }
+
+    if (this.env.platform === 'osx' && this.version.startsWith('2.4')) {
+      return 'osx-universal';
     }
 
     return `${this.env.platform}${this.env.arch}`;
